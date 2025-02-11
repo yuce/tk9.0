dcede6da453c63515eddc36a9664b1a6b42d5aff
Jan Mercl <0xjnml@gmail.com>
2025-02-11 11:41:34 +0100

    exemplify cget equivalent in _examples/cget.go, updates #38

---
+++ _examples/cget.go
@@ -0,0 +1,17 @@
+// hello.go with some padding added.
+
+package main
+
+import . "modernc.org/tk9.0"
+import _ "modernc.org/tk9.0/themes/azure"
+
+func main() {
+	btn := TButton(Txt("Hello"))
+	Pack(btn,
+	TLabel(Txt(btn.Txt())), // Tcl: .btn cget -text
+		TExit(),
+		Ipadx(10), Ipady(5), Padx(20), Pady(10))
+	ActivateTheme("azure light")
+	App.SetResizable(false, false)
+	App.Wait()
+}
