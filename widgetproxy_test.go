6f1741a58d55ad87f539d09b54a407f9646524da
Mike D Pilsbury <mike.pilsbury@gmail.com>
2025-02-25 16:22:06 +0000

    Add tests for widget proxying

---
+++ widgetproxy_test.go
@@ -0,0 +1,46 @@
+package tk9_0 // import "modernc.org/tk9.0"
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestWidgetProxy(t *testing.T) {
+	text := NewTextWidgetProxy(Text())
+
+	assertContent := func(expected string) {
+		content := text.Get("1.0", "end -1 chars")[0]
+		if expected != content {
+			t.Errorf("expected '%s', but is '%s'", expected, content)
+		}
+	}
+
+	insertCallback := func(args []string) {
+		args[2] = strings.ToUpper(args[2]) // upper case the text to be inserted
+		text.EvalWrapped(args)
+	}
+
+	// no callback registered
+	text.Insert(END, "abc ")
+	assertContent("abc ")
+
+	// callback registered
+	text.Register("insert", insertCallback)
+	text.Insert(END, "def ")
+	assertContent("abc DEF ")
+
+	// callback unregistered
+	text.Unregister("insert")
+	text.Insert(END, "ghi ")
+	assertContent("abc DEF ghi ")
+
+	// callback registered again
+	text.Register("insert", insertCallback)
+	text.Insert(END, "jkl ")
+	assertContent("abc DEF ghi JKL ")
+
+	// proxying removed
+	text.Close()
+	text.Insert(END, "mno ")
+	assertContent("abc DEF ghi JKL mno ")
+}
