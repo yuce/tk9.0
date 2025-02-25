58fd9c9dcd325d1b13c71b9e9e90fbc21a3d34f4
Jan Mercl <0xjnml@gmail.com>
2025-02-25 21:05:44 +0100

    disable TestTokenizer false positives, updates #47

--- all_test.go
+++ all_test.go
@@ -19,10 +19,6 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	if isBuilder {
-		os.Exit(0)
-	}
-
 	if Error != nil {
 		fmt.Fprintln(os.Stderr, Error)
 		os.Exit(1)
@@ -34,6 +30,14 @@ func TestMain(m *testing.M) {
 	os.Exit(rc)
 }
 
+// Commit 18c4e94e171d4 diff
+//
+// -               `([^$]|\\\$)*`,           // Not TeX, incl. "\$"
+// -               `\$([^$]|\\\$)*\$`,       // $TeX$ or $$TeX$$, incl. $Te\$X$
+// -               `\$\$?([^$]|\\\$)*\$\$?`, // $TeX$ or $$TeX$$, incl. $Te\$X$
+// +               `([^$]|\\\$)*`,         // Not TeX, incl. "\$"
+// +               `\$([^$]|\\\$)*\$`,     // $TeX$, incl. $Te\$X$
+// +               `\$\$([^$]|\\\$)*\$\$`, // $$TeX$$, incl. $Te\$X$
 func TestTokenizer(t *testing.T) {
 	for i, test := range []struct {
 		s    string
@@ -53,20 +57,20 @@ func TestTokenizer(t *testing.T) {
 		{"a\\$\\$\\$\\$", []int{0}, []string{"a\\$\\$\\$\\$"}},
 
 		{"$a$", []int{1}, []string{"$a$"}},
-		{"$$a$", []int{2}, []string{"$$a$"}},
+		// Not valid since 18c4e94e171d4 {"$$a$", []int{2}, []string{"$$a$"}},
 		{"$$a$$", []int{2}, []string{"$$a$$"}},
-		{"$a$$", []int{2}, []string{"$a$$"}},
+		// Not valid since 18c4e94e171d4 {"$a$$", []int{2}, []string{"$a$$"}},
 		{"x$a$", []int{0, 1}, []string{"x", "$a$"}},
 
-		{"x$$a$", []int{0, 2}, []string{"x", "$$a$"}},
+		// Not valid since 18c4e94e171d4 {"x$$a$", []int{0, 2}, []string{"x", "$$a$"}},
 		{"x$$a$$", []int{0, 2}, []string{"x", "$$a$$"}},
-		{"x$a$$", []int{0, 2}, []string{"x", "$a$$"}},
+		// Not valid since 18c4e94e171d4 {"x$a$$", []int{0, 2}, []string{"x", "$a$$"}},
 		{"x$a$y", []int{0, 1, 0}, []string{"x", "$a$", "y"}},
-		{"x$$a$y", []int{0, 2, 0}, []string{"x", "$$a$", "y"}},
+		// Not valid since 18c4e94e171d4 {"x$$a$y", []int{0, 2, 0}, []string{"x", "$$a$", "y"}},
 
 		{"x$$a$$y", []int{0, 2, 0}, []string{"x", "$$a$$", "y"}},
-		{"x$a$$y", []int{0, 2, 0}, []string{"x", "$a$$", "y"}},
-		{"x\\$0$a\\$1b$$\\$y", []int{0, 2, 0}, []string{"x\\$0", "$a\\$1b$$", "\\$y"}},
+		// Not valid since 18c4e94e171d4 {"x$a$$y", []int{0, 2, 0}, []string{"x", "$a$$", "y"}},
+		// Not valid since 18c4e94e171d4 {"x\\$0$a\\$1b$$\\$y", []int{0, 2, 0}, []string{"x\\$0", "$a\\$1b$$", "\\$y"}},
 	} {
 		ids, toks := tokenize(test.s)
 		if g, e := fmt.Sprintf("%v %q", ids, toks), fmt.Sprintf("%v %q", test.ids, test.toks); g != e {
@@ -74,56 +78,3 @@ func TestTokenizer(t *testing.T) {
 		}
 	}
 }
-
-// func capitalize(s string) string {
-// 	return strings.ToUpper(string(s[0])) + s[1:]
-// }
-//
-// func TestTmp(t *testing.T) {
-// 	ErrorMode = CollectErrors
-// 	themes := StyleThemeNames()
-// 	slices.Sort(themes)
-// 	for _, theme := range themes {
-// 		fmt.Printf("\n//\n// # %q theme style guide", theme)
-// 		StyleThemeUse(theme)
-// 		styles := StyleThemeStyles()
-// 		slices.Sort(styles)
-// 		elements := StyleElementNames()
-// 		slices.Sort(elements)
-// 		for _, element := range elements {
-// 			fmt.Printf("\n//\n// %q style element options:\n//", element)
-// 			options := StyleElementOptions(element)
-// 			slices.Sort(options)
-// 			for _, option := range options {
-// 				fmt.Printf("\n//  - [%s]", capitalize(option[1:]))
-// 			}
-// 		}
-// 		fmt.Printf("\n//\n// %q theme style list", theme)
-// 		needsep := false
-// 		for _, style := range styles {
-// 			if needsep {
-// 				fmt.Printf("\n//\n// -\n//\n//")
-// 			}
-// 			needsep = true
-// 			fmt.Printf("\n//\n//  %s", style)
-// 			if s := StyleLayout(style); s != "" {
-// 				needsep = false
-// 				fmt.Printf("\n//\n// Layout: %s", s)
-// 			}
-// 			if s := strings.TrimSpace(StyleMap(style)); s != "" {
-// 				needsep = false
-// 				s = strings.Join(strings.Fields(s), " ")
-// 				fmt.Printf("\n//\n// Style map: %s", s)
-// 			}
-// 		}
-// 	}
-// }
-
-// func TestKeys(t *testing.T) {
-// 	Pack(Label(Txt("Hello World!")))
-// 	Bind(App, "<Key>", Command(func(e *Event) { fmt.Printf("%q\n", e.Keysym) }))
-// 	App.Wait()
-// }
-
-// func TestTmp(t *testing.T) {
-// }
