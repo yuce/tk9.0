8aad4766d1dd61986650210740e31829cf67c049
Mike D Pilsbury <mike.pilsbury@gmail.com>
2025-02-27 08:15:13 +0000

    Use azure theme for widget proxy example

--- _examples/widgetproxy.go
+++ _examples/widgetproxy.go
@@ -8,9 +8,11 @@ import (
 
 	. "modernc.org/tk9.0"
 	_ "modernc.org/tk9.0/extensions/eval"
+	_ "modernc.org/tk9.0/themes/azure"
 )
 
 func main() {
+	ActivateTheme("azure light")
 	// Proxying widgets requires that the eval extension be enabled.
 	InitializeExtension("eval")
 
