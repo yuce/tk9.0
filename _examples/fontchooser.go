f606a96341b0a81c6f5e98894166f7762dad9002
Jan Mercl <0xjnml@gmail.com>
2025-07-31 10:17:05 +0200

    restore fontchooser.go code removed by accident

--- _examples/fontchooser.go
+++ _examples/fontchooser.go
@@ -6,5 +6,13 @@ import (
 )
 
 func main() {
+	Pack(
+		TButton(Txt("Select font..."), Command(func() {
+			Fontchooser(Command(func() { fmt.Printf("%q\n", FontchooserFont()) }))
+			FontchooserShow()
+		})),
+		TExit(),
+		Padx("1m"), Pady("2m"), Ipadx("1m"), Ipady("1m"),
+	)
 	App.Wait()
 }
